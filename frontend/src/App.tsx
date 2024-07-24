272ecd273f6f2a2062b4973431b24103226a9cbc
rafaelszp <rafaelszp.info@gmail.com>
2024-07-24 08:20:14 -0300

    codepipeline frontend trigger test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + CodeBuid + S3 + KMS + Terraform</h1>
+      <h1>Vite + React + CodeBuid + S3 +  Terraform + CodePipeline</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
