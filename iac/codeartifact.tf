420767879318dcabcbcdfc17d6719916b58d7714
rafaelszp <rafaelszp.info@gmail.com>
2024-07-31 07:26:08 -0300

    upstream repo + frontend buildspec + new frontend lib

--- iac/codeartifact.tf
+++ iac/codeartifact.tf
@@ -4,12 +4,36 @@ data "aws_kms_key" "codeartifact_kms_key" {
 
 resource "aws_codeartifact_domain" "nodejs_artifact_domain" {
   domain = "${var.department}-nodejs"
+  tags = {
+    TFName     = "nodejs_artifact_domain"
+    Department = "${var.department}"
+    Application = "${var.prefix}"
+  }
 }
 
 resource "aws_codeartifact_repository" "node_artifact_repo" {
   domain = aws_codeartifact_domain.nodejs_artifact_domain.domain
   repository = "nodejs"
+  upstream {
+   repository_name = aws_codeartifact_repository.npm_store.repository
+  }
+  tags = {
+    TFName     = "node_artifact_repo"
+    Department = "${var.department}"
+    Application = "${var.prefix}"
+  }
+}
+
+
+resource "aws_codeartifact_repository" "npm_store" {
+  domain = aws_codeartifact_domain.nodejs_artifact_domain.domain
+  repository = "npm-store"
   external_connections {
     external_connection_name = "public:npmjs"
+  }
+  tags = {
+    TFName     = "npm_store"
+    Department = "${var.department}"
+    Application = "${var.prefix}"
   }
 }
