b53108d08f4c4bcad93be24536058b04dce0e1c9
rafaelszp <rafaelszp.info@gmail.com>
2024-08-06 08:51:36 -0300

    codebuild fix

--- iac/codebuild.tf
+++ iac/codebuild.tf
@@ -129,7 +129,6 @@ resource "aws_codebuild_project" "codebuild_backend" {
   }
   
   description = "Build project for ${var.prefix}-backend"
-  build_timeout = 5
   service_role = aws_iam_role.iam_codebuild_role.arn
 
   # artifacts {
