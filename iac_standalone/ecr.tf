091eb534d199102c64cdd10787454ffd229fc3eb
rafaelszp <rafaelszp.info@gmail.com>
2024-08-09 07:34:09 -0300

    preparing for modularization

--- iac_standalone/ecr.tf
+++ iac_standalone/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "ecr_repo" {
   name = "${var.department}/${var.prefix}"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -8,6 +9,7 @@ resource "aws_ecr_repository" "ecr_repo" {
 resource "aws_ecr_repository" "ecr_repo_backend" {
   name = "${var.department}/${var.prefix}-backend"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
