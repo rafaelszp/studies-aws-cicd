ce77ef97c918b3f0f9313770590af2bde90b8c7d
rafaelszp <rafaelszp.info@gmail.com>
2024-08-17 11:08:57 -0300

    ecr lifecycle policy

--- iac/modules/devops/ecr.tf
+++ iac/modules/devops/ecr.tf
@@ -6,3 +6,40 @@ resource "aws_ecr_repository" "ecr-repo" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "ecr-lifecycle" {
+  repository = aws_ecr_repository.ecr-repo.name
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description = "Expire images older than 14 days"
+        selection = {
+          tagStatus = "untagged"
+          countType = "sinceImagePushed"
+          countUnit = "days"
+          countNumber = 14
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description = "Expire images older than 30 days with tag main-* or feature-*"
+        selection = {
+          tagStatus = "tagged"
+          tagPrefixList = ["main-", "feature-"]
+          countType = "sinceImagePushed"
+          countUnit = "days"
+          countNumber = 30
+        }
+        action = {
+          type = "expire"
+        }
+      }
+      
+    ]
+  })
+  
+}
